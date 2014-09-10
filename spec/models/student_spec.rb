f0eee8528b0a2bd86fbff5b4547bca74c77820c4
Laurie415 <Laurie@awesome>
2014-09-10 13:56:49 -0700

    1st test success for student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,2 +1,20 @@
 require 'rails_helper'
 
+describe 'Student' do
+	
+		before :each do 
+			@valid_attributes = {
+				first_name: "Sarah",
+				last_name: "Kim",
+				email: "s@s.com",
+				password: "kimdynasty",
+			}
+		end
+
+		describe 'when all attributes are valid'
+			it 'is valid' do
+				student = Student.new(@valid_attributes)
+				expect(student).to be_valid 
+			end
+		end
+
